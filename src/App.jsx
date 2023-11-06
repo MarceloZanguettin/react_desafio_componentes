60a34c80d2e74385cd249362daace954d4e398d0
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2023-11-05 21:16:35 -0300

    Desafio realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import { divide, findLastKey } from 'lodash';
 import React from 'react';
+import Header from './Header';
+import Home from './Home';
+import Produtos from './Produtos';
 
 const App = () => {
-  return <section></section>;
+  const { pathname } = window.location;
+
+  let Pagina;
+  if (pathname === '/produtos') {
+    Pagina = Produtos;
+  } else {
+    Pagina = Home;
+  }
+
+  return (
+    <section>
+      <Header />
+      <Pagina />
+    </section>
+  );
 };
 
 export default App;
